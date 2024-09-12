e5527d30a8e2c1466f4b5c3998860231e42609b0
Kalvine <kkalvine4@gmail.com>
2024-09-12 19:16:43 +0300

    full CRUD with sqlalchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -5,17 +5,120 @@ from datetime import datetime
 from sqlalchemy import (create_engine, desc, func,
     CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
     Index, Column, DateTime, Integer, String)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 
 Base = declarative_base()
 
 class Student(Base):
+    
     __tablename__ = 'students'
-
-    id = Column(Integer(), primary_key=True)
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name = "id_pk"
+        ),
+        UniqueConstraint(
+            'email',
+            name = 'unique_email'
+        ),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name = 'grade_between_1_andq_12'
+        )
+    )
+    
+    Index('index_name', 'name')
+    
+    id = Column(Integer())
     name = Column(String())
-
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+    
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
+    
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine("sqlite:///:memory:")
     Base.metadata.create_all(engine)
+    
+    
+    Session = sessionmaker(bind=engine)
+    
+    session = Session()
+    
+    
+    mystudent1 = Student(
+        name = "Albert Einstein",
+        email = "albert.einstein@zurich.edu",
+        grade = 6,
+        birthday = datetime(
+            year = 1879,
+            month = 3,
+            day = 14
+            ),
+        )
+
+    mystudent2 = Student(
+        name = "Alan Turing",
+        email = "alan.turing@zurich.edu",
+        grade = 11,
+        birthday = datetime(
+            year = 1912,
+            month = 6,
+            day = 23
+            ),
+        )
+
+    session.bulk_save_objects([mystudent1, mystudent2])
+    session.commit()
+
+    students = session.query(Student)
+    names = session.query(Student.name)
+    students_by_name = [student for student in names.order_by(Student.name)]
+    students_by_grade_desc = [student for student in session.query(
+        Student.name, Student.grade).order_by(
+            desc(Student.grade)
+            )]
+    oldest_student = session.query(
+        Student.name, Student.birthday).order_by(
+            desc(Student.grade)).first()
+    
+    student_count = session.query(func.count(Student.id)).first()
+    
+    filtered = session.query(Student).filter(Student.name.like('%Alan%'), Student.grade == 11)
+    for record in filtered:
+        print(record.name)
+        
+    session.query(Student).update({
+        Student.grade: Student.grade +1
+    })
+    
+    deleted = session.query(Student).filter(Student.name == "Albert Einstein")
+    albert = deleted.first()
+    
+    session.delete(albert)
+    session.commit()
+    
+    albert = deleted.first()
+    
+    print(albert)
+    
+    
+       
+    print([student for student in students])
+    print([name for name in names])
+    print(students_by_name)
+    print(students_by_grade_desc)
+    print(oldest_student)
+    print(student_count)
+    print([(student.name, student.grade) for student in session.query(Student)])
+
+
+    # print (f"New Student ID is {mystudent1.id} enrolled on {mystudent1.enrolled_date}.")
+    # print (f"New Student ID is {mystudent2.id} enrolled on {mystudent2.enrolled_date}.")
+
